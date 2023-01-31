34959857f33e1f3d67a713d7fa31f574ea019e93
Hatoff2 <116748960+Hatoff2@users.noreply.github.com>
2023-01-31 13:28:14 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ben Hatoff!");
+
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
+
+string text = System.IO.File.ReadAllText("text.txt");
+Console.WriteLine(text);
